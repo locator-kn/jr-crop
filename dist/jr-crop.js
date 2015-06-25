148de6efcd47ada2779ec8ce0a63791124fa2202
T1m1 <Ruprecht.t@gmx.de>
2015-06-25 12:02:39 +0200

    update dist folder

--- dist/jr-crop.js
+++ dist/jr-crop.js
@@ -225,7 +225,7 @@ function($ionicModal, $rootScope, $q) {
       canvas.width = this.options.width / this.scale;
       canvas.height = this.options.height / this.scale;
 
-      // The full proportions 
+      // The full proportions
       var currWidth = this.imgWidth * this.scale;
       var currHeight = this.imgHeight * this.scale;
 
@@ -236,6 +236,38 @@ function($ionicModal, $rootScope, $q) {
 
       var sourceX = (this.posX - correctX) / this.scale;
       var sourceY = (this.posY - correctY) / this.scale;
+
+
+      if(this.options.getOnlyData){
+        var valueX = Math.abs(sourceX);
+        var valueY = Math.abs(sourceY);
+        var cropWidth = (this.options.width * this.imgWidth) / currWidth;
+        var cropHeight = (this.options.height * this.imgHeight) / currHeight;
+
+        if(this.options.roundData) {
+          valueX = Math.floor(valueX);
+          valueY = Math.floor(valueY);
+          cropWidth = Math.floor(cropWidth);
+          cropHeight = Math.floor(cropHeight);
+          this.imgWidth = Math.floor(this.imgWidth);
+          this.imgHeight = Math.floor(this.imgHeight);
+        }
+
+        var cropInfo = {
+          x: valueX,
+          y: valueY,
+          width: cropWidth,
+          height: cropHeight,
+          originWidth: this.imgWidth,
+          originHeight: this.imgHeight,
+          src: this.imgSelect.src
+        };
+
+        this.options.modal.remove();
+        // Return without cropping file
+        return this.promise.resolve(cropInfo);
+      }
+
 
       context.drawImage(this.imgFull, sourceX, sourceY);
 
@@ -249,14 +281,14 @@ function($ionicModal, $rootScope, $q) {
      */
     loadImage: function() {
       var promise = $q.defer();
-      
+
       // Load the image and resolve with the DOM node when done.
       angular.element('<img />')
-        .bind('load', function(e) {
-          promise.resolve(this);
-        })
-        .bind('error', promise.reject)
-        .prop('src', this.options.url);
+          .bind('load', function(e) {
+            promise.resolve(this);
+          })
+          .bind('error', promise.reject)
+          .prop('src', this.options.url);
 
       // Return the promise
       return promise.promise;
@@ -269,7 +301,9 @@ function($ionicModal, $rootScope, $q) {
       height: 0,
       aspectRatio: 0,
       cancelText: 'Cancel',
-      chooseText: 'Choose'
+      chooseText: 'Choose',
+      getOnlyData: false,
+      roundData: false
     },
 
     crop: function(options) {
